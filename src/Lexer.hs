0891338492eb317c578c83c808d515a352c30ec5
Zhu Yang <zhuyang329@gmail.com>
2022-01-09 23:00:59 +0100

    implemented a simple lexer

---
+++ src/Lexer.hs
@@ -0,0 +1,19 @@
+import Data.Char (isSpace, isAlpha, isAlphaNum)
+
+data Token = Variable String
+           | Keyword String
+
+symbols = "\\.()"
+
+isIdentifierChar :: Char -> Bool
+isIdentifierChar c = isAlphaNum c || c == '_'
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c:cs)
+  | isSpace c = tokenize cs
+  | isAlpha c = let 
+      (rest, cs) = span isIdentifierChar cs
+      in Variable (c:rest) : tokenize cs
+  | c `elem` symbols = Keyword [c] : tokenize cs
+  | otherwise = error "invalid input"
