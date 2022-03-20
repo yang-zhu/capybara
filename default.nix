ee2bba437efcc8ef811161f8f42e0885462a75ce
Zhu Yang <zhuyang329@gmail.com>
2022-03-20 04:55:13 -0700

    moved the generated webpage to web branch

--- default.nix
+++
@@ -1,5 +0,0 @@
-# from the sample app in miso: https://github.com/dmjio/miso/blob/master/sample-app/default.nix
-with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/refs/tags/1.7.1.tar.gz";
-}) {});
-pkgs.haskell.packages.ghcjs.callCabal2nix "capybara" ./. {}
