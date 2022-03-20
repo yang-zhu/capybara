9bce9453dce72ff2672c8fa6e52ed242383ae7cd
Zhu Yang <zhuyang329@gmail.com>
2022-03-20 10:14:49 -0700

    added more definitions

--- src/Model.hs
+++ src/Model.hs
@@ -43,15 +43,30 @@ initialModel :: Model
 initialModel = Model
   { _termInput = ""
   , _defInput
-      = "id = λx.x;\n\
-        \True = λx.λy.x;\n\
-        \False = λx.λy.y;\n\
-        \not = λx.x False True;\n\
+      = "S = λx y z. (x z)(y z);\n\
+        \K = λx y. x;\n\
+        \I = λx. x;\n\
         \omega = (λx.(x x))(λx.(x x));\n\
         \fix = λf.(λx.(f (x x)))(λx.(f (x x)));\n\
-        \S = λx.λy.λz.(x z)(y z);\n\
-        \K = λx.λy.x;\n\
-        \I = λx.x;\n"
+        \\n\
+        \-- data Bool = True | False\n\
+        \True = λt f. t;\n\
+        \False = λt f. f;\n\
+        \not = λb. b False True;\n\
+        \and = λa b. a b False;\n\
+        \or = λa b. a True b;\n\
+        \\n\
+        \-- data Nat = Zero | Succ Nat\n\
+        \Zero = λz s.z;\n\
+        \Succ = λn z s. s n;\n\
+        \plus = λa b. a b (λa'. plus a' (Succ b));\n\
+        \mult = λa b. a zero (λa'. plus (mult a' b) b);\n\
+        \\n\
+        \-- data List a = Nil | Cons a (List a)\n\
+        \Nil = λn c.n;\n\
+        \Cons = λe es n c. c e es;\n\
+        \append = λa b. a b (λe es. Cons e (append es b));\n\
+        \nth = λl n. l omega (λe es. n e (λn'. nth es n'));\n"
   , _strategy = CallByNeed
   , _output = Output Nothing Nothing []
   }
